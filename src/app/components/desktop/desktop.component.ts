9af858448ba3e4da21052ae947931fb1fb154d59
Asedem <asedem.games@gmail.com>
2023-06-11 01:02:28 +0200

    #2 Implemented open and close mechanism for the StartMenu

--- src/app/components/desktop/desktop.component.ts
+++ src/app/components/desktop/desktop.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {subscribeOn} from "rxjs";
 
 @Component({
   selector: 'app-desktop',
@@ -8,14 +9,26 @@ import {Component, OnInit} from '@angular/core';
 export class DesktopComponent implements OnInit {
 
   date: Date = new Date();
+  startMenu?: HTMLDivElement;
+  startMenuVisibility: boolean = false;
 
   ngOnInit(): void {
     this.utcTime();
+    this.startMenu = document.getElementById('startMenu') as HTMLDivElement;
   }
 
   utcTime(): void {
     setInterval(() => {
       this.date = new Date();
     }, 1000);
+  }
+
+  toggleStartMenu() {
+    if (this.startMenuVisibility) {
+      this.startMenu!.style.display = "none";
+    } else {
+      this.startMenu!.style.display = "block";
+    }
+    this.startMenuVisibility = !this.startMenuVisibility;
   }
 }
